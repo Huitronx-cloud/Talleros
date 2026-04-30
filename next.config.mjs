707852467ca34a4c2bcaa22da03dae446c6ed101
Ivan Huitron <huitronx@icloud.com>
2026-04-30 10:47:53 -0400

    fix: next config sin duplicado

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'kbtszjpqtoqhrfnqjaxv.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-    ],
-  },
-}
-
-export default nextConfig
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -33,5 +17,4 @@ const nextConfig = {
     ],
   },
 }
-
 export default nextConfig
