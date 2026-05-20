414e4c0632ff81e6e922f4e921438ea29820b2f4
Claudio <claudio@tallerosapp.com>
2026-05-20 13:18:17 +0000

    fix: sitemap con fecha estática + JsonLd como server component puro

--- components/json-ld.tsx
+++ components/json-ld.tsx
@@ -1,9 +1,8 @@
-'use client'
-
 export function JsonLd({ data }: { data: object }) {
   return (
     <script
       type="application/ld+json"
+      // eslint-disable-next-line react/no-danger
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
   )
